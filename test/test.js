7f93f259c66c97f95aa789d7285835b6b3e59942
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-07-02 13:10:38 +0100

    refactored

--- test/test.js
+++ test/test.js
@@ -4,6 +4,9 @@ var findReplace = require("../");
 function fixture(){
     return [ 1, 2, 3, 4, 2 ];
 }
+function argv(){
+    return [ "--one", "1", "-abc", "three" ];
+}
 
 test("find primitive, replace with primitive", function(t){
     t.deepEqual(
@@ -25,6 +28,24 @@ test("find primitive, replace with several primitives", function(t){
     t.deepEqual(
         findReplace(fixture(), 2, "two", "zwei"), 
         [ 1, "two", "zwei", 3, 4, "two", "zwei" ]
+    );
+    t.end();
+});
+
+test("getopt example", function(t){
+    t.deepEqual(
+        findReplace(argv(), /^-(\w{2,})$/, function(match){
+            return [ "-a", "-b", "-c" ];
+        }), 
+        [ "--one", "1", "-a", "-b", "-c", "three" ]
+    );
+    t.end();
+});
+
+test("getopt example", function(t){
+    t.deepEqual(
+        findReplace(argv(), /^-(\w{2,})$/, "bread", "milk"), 
+        [ "--one", "1", "bread", "milk", "three" ]
     );
     t.end();
 });
